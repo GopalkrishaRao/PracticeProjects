df046c04f1027f5cb8d4d4c9f22063cac4ab75d9
GK Rao <hrgkrao@gmail.com>
2022-12-07 14:14:48 +0530

    routeing added

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Usestate from './Components/Usestate/UsestateHook';
+import {Routes, Route} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 
 function App() {
   return (
    <>
-    <Usestate/>
+   <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/usestatehook' element={<Usestate/>} />
+    </Routes>
    </>
   )
 }
---
+++ react_hooks/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom';
+
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <> 
+        <Link to={'/'}> Home </Link>
+        <Link to={'/usestatehook'}> Use State Hook </Link>
+    
+    </>
+  )
+}
