ba704d6ef5304efe9757d98b9165536c8f99ae4c
GK Rao <hrgkrao@gmail.com>
2023-02-21 23:34:03 +0530

    css added

--- React_concepts/pagination/src/components/BasicTable.js
+++ React_concepts/pagination/src/components/BasicTable.js
@@ -3,6 +3,7 @@ import { useMemo } from 'react';
 import {useTable} from 'react-table';
 import MOCK_DATA from './MOCK_DATA.json';
 import {COLUMNS} from './columns';
+import './table.css';
 
 
 export const BasicTable = () => {
