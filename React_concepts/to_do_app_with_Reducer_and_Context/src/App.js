2005529df6aa99b7705ec52e40b64392fba61f6e
GK Rao <hrgkrao@gmail.com>
2023-03-06 11:41:16 +0530

    Todo app-inputform, sending dispatch

--- React_concepts/to_do_app_with_Reducer_and_Context/src/App.js
+++ React_concepts/to_do_app_with_Reducer_and_Context/src/App.js
@@ -4,6 +4,7 @@ import {Container} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {TodoContext} from './components/context/TodoContext';
 import todoReducer from './components/context/reducer';
+import TodoForm from './components/Todoform';
 
 
 const App = ()=>{
@@ -12,6 +13,7 @@ const App = ()=>{
     <TodoContext.Provider value={{todos, dispatch}}>
       <Container fluid>
         <h1>Todo App with Context API</h1>
+        <TodoForm/>
       </Container>
     </TodoContext.Provider>
   )
