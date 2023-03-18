f6017112122210c21f4775b6a02d3c037d26cf4c
GK Rao <hrgkrao@gmail.com>
2023-03-18 22:06:13 +0530

    Todo app with react

--- React/e_commerce_app/src/index.js
+++ React/e_commerce_app/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { AppProvider } from "./context/polductcontext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+    <AppProvider>
+        <App />
+    </AppProvider>
+);
 
 
 reportWebVitals();
---
+++ Z_detial/readme.md
@@ -0,0 +1,4 @@
+# 
+## [Reference](https://www.youtube.com/watch?v=7u2Rv4HfCYQ&list=RDCMUCUSxKiac-miugK9CDsxGS9Q&start_radio=1&rv=7u2Rv4HfCYQ&t=156)
+* [Reference Git](https://github.com/coding-in-public/react-todo-app-with-local-storage)
+* [Icons-Heroicons](https://heroicons.com/)
