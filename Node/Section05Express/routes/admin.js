92429d47a22140f8a01b893dee257804dd4bb1f9
GK Rao <hrgkrao@gmail.com>
2023-09-15 19:55:21 +0530

    Module 6 templeting pug ejs

--- Node/Section05Express/routes/admin.js
+++ Node/Section05Express/routes/admin.js
@@ -3,9 +3,17 @@ const path = require('path');
 
 const router = express.Router();
 
+const products=[];
 // /admin/add-product=>get
 router.get('/add-product', (req, res, next)=>{
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    res.render('add-product', {pageTitle: 'Add Product',
+    path:'/admin/add-product',
+    formCSS: true,
+    productCSS:true,
+    activeAddProduct:true
+    });
+
     
     // res.send(`
     // <form action='/admin/add-product' method='POST'>
@@ -17,8 +25,11 @@ router.get('/add-product', (req, res, next)=>{
 
 // /admin/add-product=>post
 router.post('/add-product', (req, res, next)=>{
-    console.log(req.body);
+    // console.log(req.body);
+    products.push({title:req.body.title})
     res.redirect('/');
 });
 
-module.exports = router;
+// module.exports = router;
+exports.routes=router;
+exports.products=products;
--- Node/Section05Express/routes/shop.js
+++ Node/Section05Express/routes/shop.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get('/', (req, res, next)=>{
-    res.sendFile(path.join(__dirname, '../','views', 'shop.html'))
+    // res.sendFile(path.join(__dirname, '../','views', 'shop.html'))
+    //  use rootDir Helper function insted of above code
+    // console.log(adminData.products);
+    // res.sendFile(path.join(rootDir,'views', 'shop.html'))
+
+    // for pug
+    const products=adminData.products;
+    res.render('shop', {prods:products, pageTitle:'Shop', path:'/'})
 });
 
 module.exports = router;
