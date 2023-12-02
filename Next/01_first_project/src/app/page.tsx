b073b28c801b213de574d22e022904ec08acbab2
GK Rao <hrgkrao@gmail.com>
2023-12-02 11:26:59 +0530

    routing, meta, title, redirect path

--- Next/01_first_project/src/app/page.tsx
+++ Next/01_first_project/src/app/page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Home(){
-    return <h1>welcome home</h1>
+    return (
+        <>
+        <h1>welcome home</h1>
+        <Link href="/blog">Blog</Link>
+        <Link href="/order-product">Order</Link>
+        </>
+    )
+
 }
--- Next/01_first_project/src/app/products/[productId]/page.tsx
+++ Next/01_first_project/src/app/products/[productId]/page.tsx
@@ -1,8 +1,24 @@
+import { Metadata } from "next";
 
-export default function ProductDetails({params,}:{
-    params:{productId:string}
-}) {
-  return (
-    <div>ProductDetails {params.productId}</div>
-  )
+type Props={
+  params:{productId:string}
+};
+
+export const generateMetadata = ({params,}:Props):Metadata=>{
+  return {
+    title: `Product ${params.productId}`,
+  }
 }
+
+export default function ProductDetails({params,}:Props) {
+return (
+  <div>ProductDetails {params.productId}</div>
+)
+}
+// export default function ProductDetails({params,}:{
+//     params:{productId:string}
+// }) {
+//   return (
+//     <div>ProductDetails {params.productId}</div>
+//   )
+// }
