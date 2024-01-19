2bfdc5c70bc2329801dcd24d9ab3bbf503453a74
GK Rao <hrgkrao@gmail.com>
2024-01-19 18:01:27 +0530

    REDUX COUNTER

--- Redux/redux/src/App.jsx
+++ Redux/redux/src/App.jsx
@@ -1,10 +1,26 @@
-import {  useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux"
 
 export default function App() {
-  const counter = useSelector(state=>stete.counter)
+ const counter = useSelector((state)=>state.counter)
+
+ const dispatch = useDispatch();
+ const increment = ()=>{
+  dispatch({type:"INC"})
+ }
+ const decrement =()=>{
+  dispatch({type:"DEC"})
+ }
+ const reset = ()=>{
+  dispatch({type:"RES"})
+ }
   return (
     <>
-      <h2>Counter {counter}</h2>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      {console.log(counter)}
+      <button onClick={increment}>Increment</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={decrement}>Decrement</button>
     </>
   )
 }
