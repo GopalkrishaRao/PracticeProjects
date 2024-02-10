5d7f823a67d6758466ee1d187f74ad6fedd242e9
GK Rao <hrgkrao@gmail.com>
2024-02-10 20:43:56 +0530

    Redux counter

--- React_concepts/Redux_to_do/src/App.jsx
+++ React_concepts/Redux_to_do/src/App.jsx
@@ -1,26 +1,11 @@
-import './App.css';
-import { Container } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Todo from './components/Todo';
-import TodoForm from './components/TodoForm';
-
-import { Provider } from 'react-redux';
-import store  from './store';
-
+import Counter from "./components/counter/Counter"
 
 function App() {
 
   return (
     <>
-    <Provider store={store}>
-      <Container fluid>
-        <Todo/>
-          <TodoForm/>
-      </Container>
-    </Provider>
-      <div className="app">
-        
-      </div>
+        <h1> Redux App</h1>
+        <Counter/>
     </>
   )
 }
