439e5242cd9d222ec727ecef839f5a2eb2ac14e3
GopalkrishaRao <hrgkrao@gmail.com>
2024-03-16 11:37:11 +0530

    pagination

--- React_concepts/react_concepts/src/mini_projects/Pagination/Pagination.js
+++ React_concepts/react_concepts/src/mini_projects/Pagination/Pagination.js
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import './pagination.css';
 
-
 export default function Pagination() {
 
     const [products, setProduct]= useState([]);
-    const [page,setPage]=useState(1)
+    const [currentPage,setCurrentPage]=useState(1);
+
+    let productsPerPage=5;
+
+    let toatalNumberofPages = Math.ceil(products.length/productsPerPage);
+
+    let indexofLastProduct = currentPage * productsPerPage;
+
+    let indexofFirstProduct = indexofLastProduct-productsPerPage; 
+
+    let pages=[...Array(toatalNumberofPages + 1).keys()].slice(1);
 
     const fetchProducts = async ()=>{
         const res = await fetch("https://dummyjson.com/products");
@@ -14,18 +23,20 @@ export default function Pagination() {
             setProduct(data.products)
         };
     };
-    console.log(products);
+    
+    console.log(products.slice(indexofFirstProduct,indexofLastProduct));
 
     useEffect(()=>{
         fetchProducts();
-    },[])
+    },[]);
+
   return (
     <>
     <h2>Pagination</h2>
     { products.length ? ( 
         <div className='products'>
         {
-            products.map(product=>{
+            products.slice(indexofFirstProduct,indexofLastProduct).map(product=>{
             return(
                 <span key={product.id} className='products__single'>
                     {/* {product.title} */}
@@ -37,19 +48,34 @@ export default function Pagination() {
         }
         {
          products.length>0 && <div className='pagination'>
-            <sapn>Previous</sapn>
-            <sapn>2</sapn>
-            <sapn>3</sapn>
-            <sapn>4</sapn>
-            <sapn>Next</sapn>
+            <button onClick={()=>{
+                if(currentPage!=1)
+                setCurrentPage(currentPage-1)
+            }}>Previous</button>
 
+        <button>
+             {pages.map((page) => {
+                return (
+                  <span
+                    key={page}
+                    className={`${currentPage === page ? "active" : ""}`}
+                    onClick={() => setCurrentPage(page)}
+                  >
+                    {page} |{" "}
+                  </span>
+                );
+              })}
+        </button>
+        
+            <button onClick={()=>{
+                if(currentPage!=toatalNumberofPages)
+                setCurrentPage(currentPage+1)
+            }}>Next</button>
          </div>
         }
         </div>
         ): <h2>Loadingin.....</h2>
     }
-
     </>
-
-  )
-}
+  );
+};
